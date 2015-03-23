d2e180b2c060a1ea2a31fba307400f790e0891ed
jacobsilcoff <jacobsilcoff@gmail.com>
2015-03-23 19:04:33 -0400

    Made a js file for the site

---
+++ script.js
@@ -0,0 +1,23 @@
+/**
+* Makes an arrow open and close some text
+* @param arrow The id of the arrow being clicked (a string)
+* @param elId The id of the element being toggled (a string)
+*/
+function dropDownArrow(arrow, elId)
+{
+    var element = document.getElementById(arrow);
+    
+    console.log("element.innerHTML == " + element.innerHTML);
+    if (element.innerHTML === "❮")
+    {
+        document.getElementById(elId).style.visibility =
+            "visible";
+        element.innerHTML = "&#10095";
+    }
+    else
+    {
+        document.getElementById(elId).style.visibility =
+            "hidden";
+        element.innerHTML = "&#10094;"
+    }
+}

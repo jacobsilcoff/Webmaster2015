e05f6d065c897585cc65ef8b7a73d290a903059e
Paul F-Y <pbfy00@gmail.com>
2015-05-10 17:45:24 -0400

    asdasd

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function gen_switch(ids) {
 	$(id).style.display = "block";
     }
 }
+
+window.addEventListener('load', function(){
+	var el = document.getElementById('sitelinks');
+	var title = document.getElementById('othersites');
+	var spacer = document.getElementById('spacer');
+	var h;
+	var cs = document.defaultView.getComputedStyle(title, '');
+//	h = title.offsetHeight + parseFloat(cs.getPropertyValue('margin-top')) + parseFloat(cs.getPropertyValue('margin-bottom'))
+	window.addEventListener('scroll', function(){
+		if((window.pageYOffset || document.documentElement.scrollTop) > spacer.offsetTop){
+			spacer.style.height = el.offsetHeight + 'px';
+			el.style.position = 'fixed';
+		}else{
+			spacer.style.height = '';
+			el.style.position = '';
+		}
+	});
+});
+
